0eaa2cbd5e07bd0af55675792e06891a6f681b2e
developer2239 <42111134+developer2239@users.noreply.github.com>
2023-09-18 14:57:23 +0900

    9.2.3 springAOP(@Around) 全コントローラクラスにおける処理前後のログ出力実装

--- src/main/java/com/example/springbootsample_2/application/aspect/LogAspect.java
+++ src/main/java/com/example/springbootsample_2/application/aspect/LogAspect.java
@@ -1,7 +1,9 @@
 package com.example.springbootsample_2.application.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -33,6 +35,29 @@ public class LogAspect {
     @After("execution(* com.example.springbootsample_2.application.user.service.*.*(..))")
     public void endLog(JoinPoint jp){
         log.info("メソッド終了:"+jp.getSignature());
+    }
+
+    /**
+     * 全コントローラの実行前後にログを出力する
+     * @param jp
+     * @return
+     * @throws Throwable
+     */
+    @Around("@within(org.springframework.stereotype.Controller)")
+    public Object startLog(ProceedingJoinPoint jp) throws Throwable {
+        //開始ログ出力/ export START log
+        log.info("メソッド開始："+jp.getSignature());
+        try{
+            //メソッド実行/ run method
+            Object result =jp.proceed();
+            //終了ログ出力/ export END log
+            log.info("メソッド終了："+jp.getSignature());
+            return result;
+        }catch(Exception e) {
+            log.error("メソッド異常終了"+jp.getSignature());
+            log.error(e.toString());
+            throw e;
+        }
     }
     
 }
